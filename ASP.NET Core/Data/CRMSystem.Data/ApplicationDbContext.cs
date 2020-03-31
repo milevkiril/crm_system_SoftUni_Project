c116d7424ba7d5a35ca0e8883887399cae3fb902
milevkiril <kirilangelov.m@gmail.com>
2020-04-01 00:58:26 +0300

    Fix administration area and added Stage model.

--- ASP.NET Core/Data/CRMSystem.Data/ApplicationDbContext.cs
+++ ASP.NET Core/Data/CRMSystem.Data/ApplicationDbContext.cs
@@ -36,6 +36,8 @@
 
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<Stage> Stages { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
